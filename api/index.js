1fa5bd2d4a3ea978141ba5036ac8ce6d0bdf7f43
jallowaminatou@yahoo.com <jalllowaminatou@yahoo.com>
2024-04-28 16:42:42 -0400

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Global congiguration
+const mongoURI = process.env.MONGO_URI;
+
+
+
+
+// Connect to Mongo
+mongoose.connect(mongoURI);
+mongoose.connection.once("open", () => {
+  console.log("connected successfully to mongo");
+});
 
 // creating express application today
 const app = express();
@@ -8,5 +23,5 @@ const PORT = process.env.PORT || 5050;
 app.use(express.json());
 
 app.listen(PORT, () => {
-    console.log(`listening`);
-  });
+  console.log(`listening`);
+});
