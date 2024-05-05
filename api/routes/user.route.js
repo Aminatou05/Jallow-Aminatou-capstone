eba648e98b913ea9d30e23cdf8e1a903c9ac3fb0
jallowaminatou@yahoo.com <jalllowaminatou@yahoo.com>
2024-05-05 10:10:45 -0400

    create get user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,15 +1,21 @@
 import express from "express";
-import { test , updateUser, deleteUser} from "../controllers/user.controller.js";
+import {
+  test,
+  updateUser,
+  deleteUser,
+  getUserListings
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 // Create a new router instance
 const router = express.Router();
 
 // route for the test endpoint
-router.get('/test' , test);
+router.get("/test", test);
 
 //route for updating a user
-router.patch('/update/:id', verifyToken, updateUser)
-router.delete('/delete/:id', verifyToken, deleteUser)
+router.patch("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
+router.get('/listings/:id', verifyToken, getUserListings);
 
 export default router;
