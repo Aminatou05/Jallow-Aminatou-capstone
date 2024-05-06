b4599d1bbe1c767238115d6e821dace67e375b15
jallowaminatou@yahoo.com <jalllowaminatou@yahoo.com>
2024-05-05 23:03:51 -0400

    create edit listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,4 +1,4 @@
-import { createListing, deleteListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing , updateListing} from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 import express from "express";
@@ -10,5 +10,8 @@ router.post('/create', verifyToken, createListing);
 
 // Route to delete a listing
 router.delete('/delete/:id', verifyToken, deleteListing);
+
+// Route to update a listing
+router.post('/update/:id', verifyToken, updateListing);
 
 export default router;
